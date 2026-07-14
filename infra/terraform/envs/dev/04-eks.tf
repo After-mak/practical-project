96a7a8245a70e620942697d56f9f5e0c569ba48d
leesk0007 <leesk0007@naver.com>
2026-07-14 16:47:37 +0900

    feat: ALB 모듈 분리

--- infra/terraform/envs/dev/04-eks.tf
+++ infra/terraform/envs/dev/04-eks.tf
@@ -36,17 +36,3 @@ provider "kubernetes" {
   }
 }
 
-############################################
-# 5. ALB (Public Load Balancer)
-############################################
-module "alb" {
-  source = "../../modules/12-alb"
-
-  name   = "project03"
-  vpc_id = module.project03_vpc.vpc_id
-  public_subnet_ids = [
-    module.project03_public_subnet_a.subnet_id,
-    module.project03_public_subnet_c.subnet_id
-  ]
-  alb_sg_id = module.security_groups.alb_sg_id
-}
