df63be67e4df478349452e7531eca82315744a65
youngsik <dudtlr9308@gmail.com>
2026-07-22 16:33:39 +0900

    feat: deploy sample-fastapi through ArgoCD

--- infra/terraform/modules/16-argocd-deploy/variables.tf
+++ infra/terraform/modules/16-argocd-deploy/variables.tf
@@ -3,4 +3,25 @@ variable "grafana_admin_password" {
   type        = string
   sensitive   = true
 }
+
 variable "aws_profile" { type = string }
+
+variable "sample_fastapi_image_repository" {
+  description = "Sample FastAPI와 Worker가 사용하는 ECR Repository URL"
+  type        = string
+}
+
+variable "sample_fastapi_image_tag" {
+  description = "Sample FastAPI와 Worker가 배포할 불변 이미지 태그"
+  type        = string
+}
+
+variable "sample_fastapi_redis_host" {
+  description = "Sample FastAPI와 Worker가 연결할 ElastiCache Primary Endpoint"
+  type        = string
+}
+
+variable "sample_fastapi_redis_port" {
+  description = "Sample FastAPI와 Worker가 연결할 ElastiCache Port"
+  type        = number
+}
