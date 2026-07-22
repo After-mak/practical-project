ae7e86a67fde38800583454293c496a3dbbee0c5
songminki <songminki1@naver.com>
2026-07-22 15:41:31 +0900

    feat:변수 통합 부분 수정

--- infra/terraform/envs/dev/00_backend.tf
+++ infra/terraform/envs/dev/00_backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "tfstate-bucket-95ada58d"
+    bucket       = "tfstate-bucket-95ada58e"
     key          = "mak-tf-lock"
     region       = "ap-northeast-2"
-    profile      = "kt_cloud_infra2"
     use_lockfile = true
     encrypt      = true # tstate 에는 민감한 정보가 들어있어 암호화
   }
