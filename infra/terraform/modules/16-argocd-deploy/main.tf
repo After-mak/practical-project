f8e5039018150a088f3437c9b143dcf5b8745ce6
tkddn3901 <tkddn3901@gmail.com>
2026-07-23 15:23:43 +0900

    finops 서비스 추가

--- infra/terraform/modules/16-argocd-deploy/main.tf
+++ infra/terraform/modules/16-argocd-deploy/main.tf
@@ -233,4 +233,56 @@ spec:
 YAML
 }
 
+resource "kubectl_manifest" "finops_analyzer" {
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: finops-analyzer
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/After-mak/mak-argocd-deploy.git
+    targetRevision: main
+    path: charts/finops
+    helm:
+      values: |
+        image:
+          repository: ${var.finops_analyzer_image_repository}
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: prod
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+    - CreateNamespace=true
+YAML
+}
 
+resource "kubectl_manifest" "yelb_app" {
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: yelb
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/After-mak/mak-argocd-deploy.git
+    targetRevision: main
+    path: charts/yelb
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: yelb
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+    - CreateNamespace=true
+YAML
+}
