f018795906657f164ed0ad26fa0db26848c30989
leesk0007 <leesk0007@naver.com>
2026-07-23 15:59:07 +0900

    feat: gateway deploy repo로 이관, eks user 권한

--- infra/terraform/envs/dev/k8s/12-alb.tf
+++ infra/terraform/envs/dev/k8s/12-alb.tf
@@ -24,37 +24,6 @@ resource "kubectl_manifest" "gateway" {
   yaml_body  = file("${path.module}/gateway_api/gateway.yaml")
   depends_on = [kubectl_manifest.gatewayclass, kubectl_manifest.lb_config]
 }
-# target group 생성 ===================
-resource "kubectl_manifest" "target_group_config" {
-  yaml_body  = file("${path.module}/gateway_api/targetgroupconfigure.yaml")
-  depends_on = [kubectl_manifest.lbc_gateway_crds, kubectl_manifest.service]
-}
-
-resource "kubectl_manifest" "argocd_target_group" {
-  yaml_body  = file("${path.module}/gateway_api/argocd-targetgroup.yaml")
-  depends_on = [kubectl_manifest.lbc_gateway_crds]
-}
-# service 및 deploy 생성 ================
-resource "kubectl_manifest" "service" {
-  yaml_body  = file("${path.module}/gateway_api/service.yaml")
-  depends_on = [kubectl_manifest.gateway,kubectl_manifest.deploy]
-}
-resource "kubectl_manifest" "deploy" {
-  yaml_body  = file("${path.module}/gateway_api/deploy.yaml")
-  depends_on = [kubectl_manifest.gateway]
-}
-# route 생성 =======================
-resource "kubectl_manifest" "route" {
-  yaml_body  = file("${path.module}/gateway_api/route.yaml")
-  depends_on = [kubectl_manifest.gateway,
-                kubectl_manifest.service,
-                kubectl_manifest.target_group_config]
-}
-
-resource "kubectl_manifest" "argocd_route" {
-  yaml_body  = file("${path.module}/gateway_api/argocd-httproute.yaml")
-  depends_on = [kubectl_manifest.gateway]
-}
 
 
 # ALB 대기 (이 다음 route53을 달아야되기 때문에 pod에서 요청 넘기고 실제 alb가 생성되기까지 기다리기) 
